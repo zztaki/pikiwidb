57248c3b612a5c2bf0a89586262b616eff1ffde4
clearocean621 <130418683+clearocean621@users.noreply.github.com>
2024-04-11 21:06:29 +0800

    feat: Key cmd add Type Expire Ttl (#215)
    * 添加Type Expire Ttl命令
    
    ---------
    
    Co-authored-by: Xin.Zh <dragoncharlie@foxmail.com>

--- tests/key_test.go
+++ tests/key_test.go
@@ -109,6 +109,69 @@ var _ = Describe("Keyspace", Ordered, func() {
 		Expect(n).To(Equal(int64(0)))
 	})
 
+	It("Type", func() {
+		set := client.Set(ctx, "key", "value", 0)
+		Expect(set.Err()).NotTo(HaveOccurred())
+		Expect(set.Val()).To(Equal("OK"))
+
+		lPush := client.LPush(ctx, "mlist", "hello")
+		Expect(lPush.Err()).NotTo(HaveOccurred())
+		Expect(lPush.Val()).To(Equal(int64(1)))
+
+		sAdd := client.SAdd(ctx, "mset", "world")
+		Expect(sAdd.Err()).NotTo(HaveOccurred())
+		Expect(sAdd.Val()).To(Equal(int64(1)))
+
+		Expect(client.Type(ctx, "key").Val()).To(Equal("string"))
+		Expect(client.Type(ctx, "mlist").Val()).To(Equal("list"))
+		Expect(client.Type(ctx, "mset").Val()).To(Equal("set"))
+
+		Expect(client.Del(ctx, "key", "mlist", "mset").Err()).NotTo(HaveOccurred())
+	})
+
+	It("Expire", func() {
+		Expect(client.Set(ctx, "key_3s", "value", 0).Val()).To(Equal("OK"))
+		Expect(client.Expire(ctx, "key_3s", 3*time.Second).Val()).To(Equal(true))
+		Expect(client.TTL(ctx, "key_3s").Val()).NotTo(Equal(int64(-2)))
+
+		time.Sleep(4 * time.Second)
+		Expect(client.TTL(ctx, "key_3s").Val()).To(Equal(time.Duration(-2)))
+		Expect(client.Get(ctx, "key_3s").Err()).To(MatchError(redis.Nil))
+		Expect(client.Exists(ctx, "key_3s").Val()).To(Equal(int64(0)))
+
+		Expect(client.Do(ctx, "expire", "foo", "bar").Err()).To(MatchError("ERR value is not an integer or out of range"))
+
+	})
+
+	It("TTL", func() {
+		set := client.Set(ctx, "key1", "bcd", 10*time.Minute)
+		Expect(set.Err()).NotTo(HaveOccurred())
+		Expect(set.Val()).To(Equal("OK"))
+		Expect(client.TTL(ctx, "key1").Val()).NotTo(Equal(int64(-2)))
+
+		get := client.Get(ctx, "key1")
+		Expect(get.Err()).NotTo(HaveOccurred())
+		Expect(get.Val()).To(Equal("bcd"))
+		Expect(client.TTL(ctx, "key1").Val()).NotTo(Equal(int64(-2)))
+
+		_, err := client.Del(ctx, "key1").Result()
+		Expect(err).NotTo(HaveOccurred())
+
+		set1 := client.Set(ctx, "key1", "bcd", 10*time.Minute)
+		Expect(set1.Err()).NotTo(HaveOccurred())
+		Expect(set1.Val()).To(Equal("OK"))
+		Expect(client.TTL(ctx, "key1").Val()).NotTo(Equal(int64(-2)))
+
+		mGet := client.MGet(ctx, "key1")
+		Expect(mGet.Err()).NotTo(HaveOccurred())
+		Expect(mGet.Val()).To(Equal([]interface{}{"bcd"}))
+
+		Expect(client.TTL(ctx, "key1").Val()).NotTo(Equal(int64(-2)))
+
+		Expect(client.Expire(ctx, "key1", 1*time.Second).Val()).To(Equal(true))
+		time.Sleep(2 * time.Second)
+	})
+
 	// pikiwidb should treat numbers other than base-10 as strings
 	It("base", func() {
 		set := client.Set(ctx, "key", "0b1", 0)
@@ -240,7 +303,7 @@ var _ = Describe("Keyspace", Ordered, func() {
 		// del keys
 		Expect(client.Del(ctx, "a1", "k1", "k2", "k3", "k4", "k5").Err()).NotTo(HaveOccurred())
 	})
-    
+
 	It("should pexpire", func() {
 		Expect(client.Set(ctx, DefaultKey, DefaultValue, 0).Val()).To(Equal(OK))
 		Expect(client.PExpire(ctx, DefaultKey, 3000*time.Millisecond).Val()).To(Equal(true))
